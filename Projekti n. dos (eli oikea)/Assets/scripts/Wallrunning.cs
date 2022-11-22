da9ea70278d1bc002bfd1e1068a99f2d92336e15
Oll1 <102211270+kylmioita@users.noreply.github.com>
2022-11-22 13:55:02 +0200

    korjauksia

--- Projekti n. dos (eli oikea)/Assets/scripts/Wallrunning.cs
+++ Projekti n. dos (eli oikea)/Assets/scripts/Wallrunning.cs
@@ -1,57 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Wallrunning : MonoBehaviour
-{
-    [Header("Wallrunning")]
-    public LayerMask whatIsWall;
-    public LayerMask whatIsGround;
-    public float wallRunForce;
-    public float maxWallRunTime;
-    private float wallRunTimer;
+//public class Wallrunning : MonoBehaviour
+//{
+//    [Header("Wallrunning")]
+//    public LayerMask whatIsWall;
+//    public LayerMask whatIsGround;
+//    public float wallRunForce;
+//    public float maxWallRunTime;
+//    private float wallRunTimer;
 
-    [Header("Input")]
-    private float horizontalInput;
-    private float verticalInput;
+//    [Header("Input")]
+//    private float horizontalInput;
+//    private float verticalInput;
 
-    [Header("Detection")]
-    public float wallCheckDistance;
-    public float minJumpHeight;
-    private RaycastHit leftWallhit;
-    private RaycastHit rightWallhit;
-    private bool wallleft;
-    private bool wallright;
+//    [Header("Detection")]
+//    public float wallCheckDistance;
+//    public float minJumpHeight;
+//    private RaycastHit leftWallhit;
+//    private RaycastHit rightWallhit;
+//    private bool wallleft;
+//    private bool wallright;
 
-    [Header("References")]
-    public Transform orientation;
-    private PlayerMovement pm;
-    private Rigidbody rb; 
+//    [Header("References")]
+//    public Transform orientation;
+//    private PlayerMovement pm;
+//    private Rigidbody rb; 
 
-    private void Start()
-    {
-        rb = GetComponent<RigidBody>();
-        pm = GetComponent<PlayerMovement>();
-    }
+//    private void Start()
+//    {
+//        rb = GetComponent<RigidBody>();
+//        pm = GetComponent<PlayerMovement>();
+//    }
 
-    private void update()
-    {
-        CheckForWall();
-    }
-    private void CheckForWall()
-    {
-        wallright = Physics.Raycast(transform.position, orientation.right, out rightWallhit, wallCheckDistance, whatIsWall);
-        wallleft = Physics.Raycast(transform.position, orientation.right, out leftWallhit, wallCheckDistance, whatIsWall);
-    }
-    private bool AboveGround()
-    {
-        return !Physics.Raycast(transform.position, Vector3.down, minJumpHeight, whatIsGround);
-    }
-    private void StateMachine()
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-        if((wallleft ))
+//    private void update()
+//    {
+//        CheckForWall();
+//    }
+//    private void CheckForWall()
+//    {
+//        wallright = Physics.Raycast(transform.position, orientation.right, out rightWallhit, wallCheckDistance, whatIsWall);
+//        wallleft = Physics.Raycast(transform.position, orientation.right, out leftWallhit, wallCheckDistance, whatIsWall);
+//    }
+//    private bool AboveGround()
+//    {
+//        return !Physics.Raycast(transform.position, Vector3.down, minJumpHeight, whatIsGround);
+//    }
+//    private void StateMachine()
+//        horizontalInput = Input.GetAxisRaw("Horizontal");
+//        verticalInput = Input.GetAxisRaw("Vertical");
+//        if((wallleft ))
 
 
 
-}
+//}
