476f0e68117fb491596ae2d413f3ba822453f251
s1save00 <100769670+s1save00@users.noreply.github.com>
2022-11-29 12:34:19 +0200

    29.11
    kona

--- Projekti n. dos (eli oikea)/Assets/scripts/PlayerCam.cs
+++ Projekti n. dos (eli oikea)/Assets/scripts/PlayerCam.cs
@@ -3,36 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCam : MonoBehaviour
-
 {
     public float sensX;
     public float sensY;
 
     public Transform orientation;
+    public Transform camHolder;
 
-    float xRotation; 
+    float xRotation;
     float yRotation;
 
     private void Start()
-
     {
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false; 
+        Cursor.visible = false;
     }
+
     private void Update()
     {
-        // Mouse input
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX; 
+        // get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-        yRotation += mouseX; 
+        yRotation += mouseX;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -60f, 70f);
 
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        // rotate cam and orientation
+        camHolder.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
-
-
     }
+
+
 }
